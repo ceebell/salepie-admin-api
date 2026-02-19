efa5e7bded3617b0fc34b34a6c0004c721fb35c6
support-alex <support.alex@alexrental.app>
2026-02-20 01:39:28 +0700

    fix CRLF LF problem

--- deploy.sh
+++ deploy.sh
@@ -1,38 +1,17 @@
-# $ docker service create \
-#     --name=myservice \
-#     --mount type=volume,source=test-data,target=/somewhere \
-#     nginx:alpine
+#!/bin/bash
 
-# myservice
+echo "Starting Deployment..."
 
-# $ docker service update \
-#     --mount-add type=volume,source=other-volume,target=/somewhere-else \
-#     myservice
+# 1. เข้าไปที่โฟลเดอร์โปรเจกต์
+cd /home/ubuntu/salepie-admin-api
 
-# myservice
+# 2. ดึงโค้ดล่าสุดจาก Git
+git pull origin main
 
-# $ docker service update --mount-rm /somewhere myservice
+# 3. ติดตั้ง Dependencies ใหม่ (เผื่อมีการเพิ่ม library ใน requirements.txt)
+/home/ubuntu/salepie-admin-api/venv/bin/pip install -r requirements.txt
 
-# myservice
+# 4. Restart Systemd Service
+sudo systemctl restart salepie.service
 
-
-# https://docs.docker.com/storage/volumes/
-
-# $ docker service create \
-#     --mount 'type=volume,src=<VOLUME-NAME>,dst=<CONTAINER-PATH>,volume-driver=local,volume-opt=type=nfs,volume-opt=device=<nfs-server>:<nfs-path>,"volume-opt=o=addr=<nfs-address>,vers=4,soft,timeo=180,bg,tcp,rw"'
-#     --name myservice \
-#     <IMAGE>
-
-
-# $ docker volume inspect my-vol
-
-# $ docker service create -d \
-#   --replicas=4 \
-#   --name devtest-service \
-#   --mount source=myvol2,target=/app \
-#   nginx:latest
-
-
-docker build  . -f Dockerfile -t apiimage:2
-
-sudo docker service create   --name apiimage   --replicas=1  --mount source=thelogs,target=/Users/belliecee/testlog  -d -p 8079:80  apiimage:2
+echo "Deployment Completed!"
