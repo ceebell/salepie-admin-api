153c580829a1595cae2236fb3c84e2765be9526a
support-alex <support.alex@alexrental.app>
2026-02-20 11:05:15 +0700

    update ver 0.0.10

--- rungit.bat
+++ rungit.bat
@@ -1,10 +1,18 @@
 
+@REM echo "GIT checkout at dev"
+git checkout dev
+
+@REM echo "GIT pull"
+git pull
+
 @REM echo "Enter the message for git commit : "
 
 set /p cm=Please put the message :  
+
+
 
 git add .
 
 git commit -m %cm%
 
-git push
+git push origin dev
